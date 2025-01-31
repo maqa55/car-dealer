8c3fa0ff6173f090d96737b7d84bbc8f62c38b24
maqa55 <maqamet8@gmail.com>
2025-01-31 17:54:03 +0400

    car commit

--- js/car.js
+++ js/car.js
@@ -1,34 +1,171 @@
 "use strict";
 
-const activElements = document.querySelectorAll(".telul .activ");
+const bars = document.querySelectorAll(".hed .bars");
+const telul = document.querySelectorAll(".telul");
 
-activElements.forEach(function (activ) {
-    activ.addEventListener("click", function () {
-        const nestedUl = activ.parentElement.querySelector("ul");
-
-        nestedUl.classList.toggle("hom");
+bars.forEach(bar => {
+    bar.addEventListener("click", function () {
+        telul.forEach(tel => {
+            tel.classList.toggle("telnav");
+        });
     });
 });
 
-// const nav = document.querySelector(".hed .nav");
-// const tel = document.querySelector(".tel .telul");
+const menuItems = document.querySelectorAll('.tela');
 
-// nav.addEventListener("click", function () {
-//     tel.classList.toggle("telnav")
-// })
-const new2 = document.querySelector(".new2 .ir")
-const new22 = document.querySelector(".new2 ul li ul")
+menuItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.stopPropagation();  
 
-new2.addEventListener("click", function () {
-    new22.classList.toggle("neww2")
-})
+        event.preventDefault();  
 
-const new3 = document.querySelector(".new3 .bloga")
-const new4 = document.querySelector(".new3 .ir")
+        const subMenu = this.querySelector('ul');
+        const icon = this.querySelector('i');
 
-new4.addEventListener("click", function () {
-    new4.classList.toggle("new3")
-})
+        if (subMenu.style.display === 'block') {
+            subMenu.style.display = 'none';
+            icon.classList.remove('active');
+        } else {
+            document.querySelectorAll('.tela ul').forEach(sub => {
+                sub.style.display = 'none';  
+            });
+
+            subMenu.style.display = 'block';
+            icon.classList.add('active');
+        }
+    });
+});
+
+const new2Items = document.querySelectorAll('.new2');
+
+new2Items.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();  
+
+        const subMenu = this.querySelector('ul');
+        const icon = this.querySelector('i');
+
+        if (subMenu && subMenu.style.display === 'block') {
+            subMenu.style.display = 'none';
+            icon.classList.remove('active');
+        } else {
+            document.querySelectorAll('.new2 ul').forEach(sub => {
+                sub.style.display = 'none'; 
+            });
+
+            if (subMenu) {
+                subMenu.style.display = 'block';
+                icon.classList.add('active');
+            }
+        }
+    });
+});
+
+
+
+const new3Items = document.querySelectorAll('.new3');
+
+new3Items.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();  
+        event.stopPropagation();  
+
+        const subMenu = this.querySelector('ul');
+        const subSubMenu = this.querySelector('.bloga');
+        const icon = this.querySelector('i');
+
+        if (subMenu && subMenu.style.display === 'block') {
+            subMenu.style.display = 'none';
+            icon.classList.remove('active');
+        } else {
+            document.querySelectorAll('.new3 ul').forEach(sub => {
+                sub.style.display = 'none';  
+            });
+
+            if (subMenu) {
+                subMenu.style.display = 'block';
+                icon.classList.add('active');
+            }
+
+            if (subSubMenu && subSubMenu.style.display === 'none') {
+                subSubMenu.style.display = 'block';
+            } else if (subSubMenu) {
+                subSubMenu.style.display = 'none';
+            }
+        }
+    });
+});
+
+
+const blogliItems = document.querySelectorAll('.blogli');
+
+blogliItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.stopPropagation(); 
+        event.preventDefault();   
+
+        const subMenu = this.querySelector('ul');
+        const icon = this.querySelector('i');
+
+        if (subMenu && subMenu.style.display === 'block') {
+            subMenu.style.display = 'none';
+            icon.classList.remove('active');
+        } else {
+            document.querySelectorAll('.blogli ul').forEach(sub => {
+                sub.style.display = 'none';  
+            });
+
+            if (subMenu) {
+                subMenu.style.display = 'block';
+                icon.classList.add('active');
+            }
+        }
+    });
+});
+
+
+
+const liaaItems = document.querySelectorAll('.liaa');
+
+liaaItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.stopPropagation();  
+        event.preventDefault();   
+
+        const subMenu = this.querySelector('ul');
+
+        if (subMenu) {
+            if (subMenu.style.display === 'none' || subMenu.style.display === '') {
+                subMenu.style.display = 'block';
+            } else {
+                subMenu.style.display = 'none';
+            }
+        }
+    });
+});
+
+
+
+const blItems = document.querySelectorAll('.bl');
+
+blItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.stopPropagation();  
+        event.preventDefault();   
+
+        const subMenu = this.querySelector('ul');
+
+        if (subMenu) {
+            subMenu.classList.toggle('bll');
+        }
+    });
+});
+
+
+
+
+
+
 
 
 
